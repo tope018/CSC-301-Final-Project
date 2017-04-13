27952330c26fb20be32d63ea2d314941acbf6fc8
tope018 <tope018@gmail.com>
2017-04-12 20:03:15 -0400

    Basic Home page with images

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="index.css">
+    <title>
+        Home Page
+    </title>
+    <style>
+        .textwrap {
+            float: right;
+            margin: 10px;
+        }
+    </style>
+    <meta charset="utf-8">    
+</head>
+
+<body>
+    <div class="left-box">
+        <div class="icon">
+            <img src="kalista%20icon.png" alt="Kalista Icon"
+                 style="width:200px; height:200px">
+        </div>
+        <ul class="side-nav" role="menubar" title="Link List">
+            <li role="menuitem"><a class="nav" href="index.php">Home</a></li>
+            <li role="menuitem"><a class="nav" href="index.php">Add Champion</a></li>
+            <li role="menuitem"><a class="nav" href="index.php">Edit Champion</a></li>
+            <li role="menuitem"><a class="nav" href="index.php">View Winrate</a></li>        
+        </ul>
+    </div>
+    <div class="right-box">
+        <div class="wrapper">
+        <h1>Keep Track Of Your Champs</h1>
+        <br>
+        <img src="ekko.jpg" style="width:297px; height:167px">
+        <img src="caitlyn.jpg" style="width:297px; height:167px">
+        <img src="braum.jpg" style="width:297px; height:167px">
+        <img src="ahri.jpg" style="width:297px; height:167px">
+        <br>
+        <h2>Champion Info At Your FingerTips</h2>
+        <p>
+            Playing champions is fun, but how do your different champions compare?
+            Which champion do you play the best? Which champion do you play the worst?
+            Through the use of this website, you are able to input any champion you play,
+            along with updating how many times you win and lose for each champion. Using the
+            side navigation bar, you can view your winrates for each champion, allowing you to
+            see which champions you perform best with.
+        </p>
+        </div>
+    </div>
+    
+</body>
+</html>
